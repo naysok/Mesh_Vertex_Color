09ba44c7e5b011f781e8fa419bb5b69022122aa3
naysok_win10 <ysok.ysok@gmail.com>
2020-09-04 22:41:42 +0900

    [add] Mesh Segment In/Out

---
+++ mesh_vertex_color/mesh_point_inside_outside.py
@@ -0,0 +1,65 @@
+import math
+
+from . import ray_triangle_intersection
+
+reload(ray_triangle_intersection)
+rt = ray_triangle_intersection.RayTriangleIntersection()
+
+
+class MeshPointInsideOutside():
+
+    """
+
+    mesh = [[v0.X, v0.Y, v0.Z], [v1.X, v1.Y, v1.Z], [v2.X, v2.Y, v2.Z]]
+    
+
+    """
+
+    def mesh_intersect(self, mesh, point):
+        
+        ### ray_triangle_intersection
+        ### rt.calc_intersection(o, d, v0, v1, v2)
+        
+        o = [0, 0, 0]
+        d = point
+        v0 = mesh[0]
+        v1 = mesh[1]
+        v2 = mesh[2]
+
+        bool_intersect = rt.calc_intersection(o, d, v0, v1, v2)
+        
+        return bool_intersect
+
+
+    def poly_mesh_intersection(self, poly_mesh, point):
+        
+        in_out_count = 0
+
+        for i in range(len(poly_mesh)):
+            
+            tmp_mesh = poly_mesh[i]
+            in_out_bool = self.mesh_intersect(tmp_mesh, point)
+
+            if in_out_bool != None:
+                in_out_count += 1
+            
+        # print(in_out_count)
+        return in_out_count
+
+
+    def mesh_points_segmentation(self, poly_mesh, points):
+
+        points_bool = []
+        
+        for i in range(len(points)):
+
+            tmp_pt = points[i]
+            inout_count = self.poly_mesh_intersection(poly_mesh, tmp_pt)
+
+            if inout_count%2 == 1:
+                points_bool.append(True)
+            else:
+                points_bool.append(False)
+        
+        return points_bool
+
