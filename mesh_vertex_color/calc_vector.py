82d82a81f5d2c4511d17b09861df2f9c645bfa12
naysok_win10 <ysok.ysok@gmail.com>
2020-09-04 21:16:52 +0900

    [update] cast_float

--- mesh_vertex_color/calc_vector.py
+++ mesh_vertex_color/calc_vector.py
@@ -4,13 +4,22 @@ class CalcVector():
 
 
     def vector_length(self, vec):
-        pass        
+        
+        length = math.sqrt(
+            math.pow(float(vec[0]), 2) + 
+            math.pow(float(vec[1]), 2) + 
+            math.pow(float(vec[2]), 2))
+        
+        return length
+
 
     def vector_add(self, va, vb):
+
         new_vec = [
             float(va[0]) + float(vb[0]),
             float(va[1]) + float(vb[1]),
             float(va[2]) + float(vb[2])]
+        
         return new_vec
 
 
@@ -19,41 +28,49 @@ class CalcVector():
         vvv = self.vector_add(vv, vc)
         return vvv
 
+    
+    def vector_subtract(self, va, vb):
+
+        new_vec = [
+            float(va[0]) - float(vb[0]),
+            float(va[1]) - float(vb[1]),
+            float(va[2]) - float(vb[2])]
+        
+        return new_vec
+
 
     def vector_multiplicate(self, vec, value):
+
         new_vec = [
             float(vec[0]) * value,
             float(vec[1]) * value,
             float(vec[2]) * value]
+
         return new_vec
 
 
     def vector_unitize(self, vec):
-        length = math.sqrt(
-            math.pow(float(vec[0]), 2) + 
-            math.pow(float(vec[1]), 2) + 
-            math.pow(float(vec[2]), 2))
+
+        length = self.vector_length(vec)
         new_vec = self.vector_multiplicate(vec, (1.0 / length))
+
         return new_vec
 
 
     def vector_cross(self, va, vb):
-        
-        n0 = va[1] * vb[2] - va[2] * vb[1]
-        n1 = va[2] * vb[0] - va[0] * vb[2]
-        n2 = va[0] * vb[1] - va[1] * vb[0]
+
+        n0 = (float(va[1]) * float(vb[2])) - (float(va[2]) * float(vb[1]))
+        n1 = (float(va[2]) * float(vb[0])) - (float(va[0]) * float(vb[2]))
+        n2 = (float(va[0]) * float(vb[1])) - (float(va[1]) * float(vb[0]))
 
         vec = [n0, n1, n2]
 
-        ### Unitizing
-        new_vec = self.vector_unitize(vec)
-
-        return new_vec
+        return vec
 
 
     def vector_dot(self, va, vb):
 
-        dot_val = va[0] * vb[0] + va[1] * vb[1] + va[2] * vb[2]
+        dot_val = (float(va[0]) * float(vb[0])) + (float(va[1]) * float(vb[1])) + (float(va[2]) * float(vb[2]))
 
         return dot_val
 
