db4a5c32e8207e641379cf3978cce9305b9c597f
naysok <ysok.ysok@gmail.com>
2020-10-14 03:24:49 +0900

    [update] numpy test

--- mesh_vertex_color/np_mesh_point_inside_outside.py
+++ mesh_vertex_color/np_mesh_point_inside_outside.py
@@ -5,8 +5,9 @@ import numpy as np
 
 ### Python3
 ### Using Numpy
-from mesh_vertex_color import np_ray_triangle_intersection
+from mesh_vertex_color import np_ray_triangle_intersection, util
 rt = np_ray_triangle_intersection.RayTriangleIntersection()
+ut = util.Util()
 
 
 class MeshPointInsideOutside():
@@ -14,38 +15,65 @@ class MeshPointInsideOutside():
 
     """
     
-    mesh = [[v0.X, v0.Y, v0.Z], [v1.X, v1.Y, v1.Z], [v2.X, v2.Y, v2.Z]]
+    poly_mesh = 
+    [
+        [
+            [v0.X, v0.Y, v0.Z],
+            [v1.X, v1.Y, v1.Z], 
+            [v2.X, v2.Y, v2.Z]
+        ],        
+        [
+            [v0.X, v0.Y, v0.Z],
+            [v1.X, v1.Y, v1.Z], 
+            [v2.X, v2.Y, v2.Z]
+        ],
+        [
+            [v0.X, v0.Y, v0.Z],
+            [v1.X, v1.Y, v1.Z], 
+            [v2.X, v2.Y, v2.Z]
+        ],
+    ]
+
+
+    mesh = 
+    [
+        [v0.X, v0.Y, v0.Z],
+        [v1.X, v1.Y, v1.Z], 
+        [v2.X, v2.Y, v2.Z]
+    ]
     
     """
 
 
-    def mesh_intersect(self, mesh, point):
+    def poly_mesh_intersection(self, poly_mesh, point):
         
         ### Optimization with Numpy
 
-        ### ray_triangle_intersection
-        ### rt.calc_intersection(o, d, v0, v1, v2)
-        
-        o = [0, 0, 0]
-        d = point
-        v0 = mesh[0]
-        v1 = mesh[1]
-        v2 = mesh[2]
+        ### Transpose Matrix
+        ### Nx3 >>> 3xN 
+        poly_mesh_t = ut.transpose_matrix(poly_mesh)
 
-        # bool_intersect = rt.calc_intersection(o, d, v0, v1, v2)
-        
-        return bool_intersect
+        o = np.array([0, 0, 0]).reshape(1, 3)
+        # o = o.reshape(1, 3)
+        d = np.array(point).reshape(1, 3)
+        # d = d.reshape(1, 3)
+        v0 = np.array(poly_mesh_t[0])
+        v1 = np.array(poly_mesh_t[1])
+        v2 = np.array(poly_mesh_t[2])
 
+        # print("o : {}".format(o))
+        # print("o.shape : {}".format(o.shape))
 
-    def poly_mesh_intersection(self, poly_mesh, points):
+        # print("d : {}".format(d))
+        # print("d.shape : {}".format(d.shape))
+
+        # print("v0 : {}".format(v0))
+        # print("v0.shape : {}".format(v0.shape))
+
+        ### Calc Intersection
+        intersect_count = rt.calc_intersection(o, d, v0, v1, v2)
         
-        ### Optimization with Numpy
-        
-        # if intersect_count%2 == 1:
-        #     new_list.append(white)
-        # else:
-        #     new_list.append(blank)
-        pass
+        return intersect_count
 
 
     def mesh_points_segmentation(self, poly_mesh, points):
@@ -64,4 +92,3 @@ class MeshPointInsideOutside():
                 points_bool.append(False)
         
         return points_bool
-
