1351b719ae9b80ced6d21a17c1bb9d7287d24a00
naysok <ysok.ysok@gmail.com>
2020-10-13 21:36:40 +0900

    [add] util

---
+++ mesh_vertex_color/util.py
@@ -0,0 +1,18 @@
+import datetime
+import math
+
+
+class Util():
+    
+    
+    def get_current_time(self):
+        return str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+
+
+    def remap_number(self, src, old_min, old_max, new_min, new_max):
+        return ((src - old_min) / (old_max - old_min) * (new_max - new_min) + new_min)
+
+
+    def zip_matrix(self, mat):
+        ### https://note.nkmk.me/python-list-transpose/
+        return [list(x) for x in zip(*mat)]
